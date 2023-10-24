d1aaa53d139b03b201603830125b03fa640162a4
anam236razaq <101165022+anam236razaq@users.noreply.github.com>
2023-10-24 16:52:08 +0500

    Update FormData-New.js

--- src/Component/FormData-New.js
+++ src/Component/FormData-New.js
@@ -1,59 +1,70 @@
-import React, { useState } from 'react'
-import { useEffect } from 'react'
+import React, { Component } from 'react'
 
-export default function FormData({AddData, editData, setEditData}) {
-  const[firstName, setFirstName]=useState("");
-  const[lastName, setLastName]=useState("");
-  const[phoneNo, setPhoneNo]=useState("");
-  const[address, setAddress]=useState("");
+const initialState={
+  firstName: "",
+  lastName: "",
+  phoneNo: "",
+  address: "",
+}
 
-  useEffect(()=>{
-   if(editData){
-    setFirstName(editData.firstName);
-    setLastName(editData.lastName);
-    setPhoneNo(editData.phoneNo);
-    setAddress(editData.address);
-   }}, [editData]);
+export default class FormData extends Component {
+  constructor(props){
+    super(props);
+    this.state={...initialState};
+  }
 
-   function handleClearForm(){
-     setFirstName("");
-     setLastName("");
-     setPhoneNo("");
-     setAddress("");
-   }
-
-  function handleSubmit(e){
-    e.preventDefault();
-    if(!firstName && !lastName && !phoneNo && !address) return;
-    const newItem={firstName, lastName, phoneNo, address, id: Date.now() };
-    AddData(newItem);
-    handleClearForm();
-    }
-
+  componentDidUpdate(prevProps){
+   const{ editData }= this.props;
+   const prevEditData=prevProps.editData;
    
-    function handleCancel(){
-      setEditData(null);
-      handleClearForm();
+   if(editData && editData!==prevEditData){
+   this.setState({
+    firstName: editData.firstName || "",
+    lastName: editData.lastName || "",
+    phoneNo: editData.phoneNo ||"",
+    address: editData.address || "",
+   })
+   }}
+
+  handleClearForm=()=>{
+     this.setState({...initialState})};
+
+   handleSubmit=(e)=>{
+    e.preventDefault();
+    const{firstName, lastName, phoneNo, address}=this.state;
+    if(!firstName && !lastName && !phoneNo && !address) return;
+    const newItem={ ...this.state, id: Date.now() };
+    this.props.AddData(newItem);
+    this.handleClearForm();
     }
 
-  return (
-    <div>
-         <form className="form" onSubmit={handleSubmit}>
+   handleCancel=()=>{
+      this.props.setEditData(null);
+      this.handleClearForm();
+    }
+
+  render() {
+    const {firstName, lastName, phoneNo, address}=this.state;
+    const {editData}=this.props;
+    return (
+          <div>
+         <form className="form" onSubmit={this.handleSubmit}>
             <label>FirstName</label>
             <input type="text" placeholder="FirstName" value={firstName} 
-            onChange={(e)=>setFirstName(e.target.value)}/>
+            onChange={(e)=>this.setState({firstName: e.target.value})}/>
             <label>LastName</label>
             <input type="text" placeholder="LastName" value={lastName} 
-            onChange={(e)=>setLastName(e.target.value)}/>
+            onChange={(e)=>this.setState({lastName: e.target.value})}/>
             <label>PhoneNo</label>
             <input type="text" placeholder="PhoneNo" value={phoneNo} 
-            onChange={(e)=>setPhoneNo(e.target.value)}/>
+            onChange={(e)=>this.setState({phoneNo: e.target.value})}/>
             <label>Address</label>
             <input type="text" placeholder="Address"  value={address} 
-            onChange={(e)=>setAddress(e.target.value)}/>
+            onChange={(e)=>this.setState({address: e.target.value})}/>
             <button className="btn">{editData? "Edit": "Insert"}</button>
           </form>
-          {editData  && <button className="cancel-btn" onClick={handleCancel}>Cancel</button>}
+          {editData  && <button className="cancel-btn" onClick={this.handleCancel}>Cancel</button>}
     </div>
-  )
+    )
+  }
 }
