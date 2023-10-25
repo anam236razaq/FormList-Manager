5c728180da9b2c028fa93a1d7bcae11ee0298bd3
anam236razaq <101165022+anam236razaq@users.noreply.github.com>
2023-10-25 12:03:35 +0500

    Update FormData-New.js

--- src/Component/FormData-New.js
+++ src/Component/FormData-New.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const initialState={
   firstName: "",
@@ -7,64 +7,64 @@ const initialState={
   address: "",
 }
 
-export default class FormData extends Component {
-  constructor(props){
-    super(props);
-    this.state={...initialState};
+function FormData({editData, setEditData, AddData}){
+  const [formData, setFormData]=useState({...initialState});
+
+  useEffect(()=>{
+    if(editData){
+       setFormData({
+           firstName: editData.firstName || "",
+           lastName: editData.lastName || "",
+           phoneNo: editData.phoneNo || "",
+           address: editData.address || ""});
+    }
+   }, [editData]);
+
+   function updateField(name, value){
+     setFormData((prevData)=>({
+       ...prevData,
+       [name]: value,
+     }));
+   }
+
+  function handleClearForm(){
+     setFormData({...initialState});
+ }
+
+  function handleSubmit(e){
+  e.preventDefault();
+  const{firstName, lastName, phoneNo, address}=formData;
+  if(!firstName && !lastName && !phoneNo && !address) return;
+  const newItem={ ...formData, id: Date.now() };
+  AddData(newItem);
+  handleClearForm();
   }
 
-  componentDidUpdate(prevProps){
-   const{ editData }= this.props;
-   const prevEditData=prevProps.editData;
+  function handleCancel(){
+    setEditData(null);
+    handleClearForm();
+  }
+
+  return (
+    <div>
+   <form className="form" onSubmit={handleSubmit}>
+      <label>FirstName</label>
+      <input type="text" placeholder="FirstName" value={formData.firstName} 
+      onChange={(e)=>updateField("firstName", e.target.value)}/>
+      <label>LastName</label>
+      <input type="text" placeholder="LastName" value={formData.lastName} 
+      onChange={(e)=>updateField("lastName", e.target.value)}/>
+      <label>PhoneNo</label>
+      <input type="text" placeholder="PhoneNo" value={formData.phoneNo} 
+      onChange={(e)=>updateField("phoneNo", e.target.value)}/>
+      <label>Address</label>
+      <input type="text" placeholder="Address"  value={formData.address} 
+      onChange={(e)=>updateField("address", e.target.value)}/>
+      <button className="btn">{editData? "Edit": "Insert"}</button>
+    </form>
+    {editData  && <button className="cancel-btn" onClick={handleCancel}>Cancel</button>}
+</div>
+)}
+
+export default FormData
    
-   if(editData && editData!==prevEditData){
-   this.setState({
-    firstName: editData.firstName || "",
-    lastName: editData.lastName || "",
-    phoneNo: editData.phoneNo ||"",
-    address: editData.address || "",
-   })
-   }}
-
-  handleClearForm=()=>{
-     this.setState({...initialState})};
-
-   handleSubmit=(e)=>{
-    e.preventDefault();
-    const{firstName, lastName, phoneNo, address}=this.state;
-    if(!firstName && !lastName && !phoneNo && !address) return;
-    const newItem={ ...this.state, id: Date.now() };
-    this.props.AddData(newItem);
-    this.handleClearForm();
-    }
-
-   handleCancel=()=>{
-      this.props.setEditData(null);
-      this.handleClearForm();
-    }
-
-  render() {
-    const {firstName, lastName, phoneNo, address}=this.state;
-    const {editData}=this.props;
-    return (
-          <div>
-         <form className="form" onSubmit={this.handleSubmit}>
-            <label>FirstName</label>
-            <input type="text" placeholder="FirstName" value={firstName} 
-            onChange={(e)=>this.setState({firstName: e.target.value})}/>
-            <label>LastName</label>
-            <input type="text" placeholder="LastName" value={lastName} 
-            onChange={(e)=>this.setState({lastName: e.target.value})}/>
-            <label>PhoneNo</label>
-            <input type="text" placeholder="PhoneNo" value={phoneNo} 
-            onChange={(e)=>this.setState({phoneNo: e.target.value})}/>
-            <label>Address</label>
-            <input type="text" placeholder="Address"  value={address} 
-            onChange={(e)=>this.setState({address: e.target.value})}/>
-            <button className="btn">{editData? "Edit": "Insert"}</button>
-          </form>
-          {editData  && <button className="cancel-btn" onClick={this.handleCancel}>Cancel</button>}
-    </div>
-    )
-  }
-}
